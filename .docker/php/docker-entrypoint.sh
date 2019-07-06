c58d52645537ddbf92da3d8dbf0932edb27c3a26
sybernatuz <decultot.louis@gmail.com>
2019-07-06 12:34:58 +0200

    docker prod

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -7,11 +7,15 @@ if [ "${1#-}" != "$1" ]; then
 fi
 
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
-	if [ "$APP_ENV" != 'prod' ]; then
-		composer install --prefer-dist --no-progress --no-suggest --no-interaction
-		bin/console assets:install
-		bin/console doctrine:schema:update -f
-	fi
+    if [ "$APP_ENV" != 'prod' ]; then
+        composer install
+    fi
+    if [ "$APP_ENV" == 'prod' ]; then
+        composer install --no-dev
+    fi
+    bin/console assets:install
+    bin/console doctrine:schema:update -f
+    bin/console doctrine:fixtures:load --group=demo
 
 	# Permissions hack because setfacl does not work on Mac and Windows
 	chown -R www-data var
--- config/bundles.php
+++ config/bundles.php
@@ -4,7 +4,7 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
